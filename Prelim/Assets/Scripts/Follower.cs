cd4453acb6f632effdf860e935435c1ca87bc35d
Kyrilios <Cyriljohnamoronio@outlook.com>
2020-08-29 16:38:08 +0800

    initial

---
+++ Prelim/Assets/Scripts/Follower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class Follower : MonoBehaviour
+{
+        public PathCreator pathCreator;
+        public float speed;
+        float distanceTravelled;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        distanceTravelled += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
+    }
+}
